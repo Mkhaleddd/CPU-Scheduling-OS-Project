4fb33fea617932f6b96719847bb585e792f11c1c
MannehAlkurdi <mannehalkurdi@gmail.com>
2026-04-15 23:43:16 +0200

    Update gui.h

--- include/gui.h
+++ include/gui.h
@@ -3,14 +3,16 @@
 #include <QTimer>
 #include <QTableWidget>
 #include <QLabel>
-#include <QPushButton>
 #include <QGraphicsView>
 #include <QGraphicsScene>
 #include <QLineEdit>    
 #include <QComboBox>    
+#include <QPushButton>   
+#include <queue>
 
 class SchedulerGUI : public QWidget {
     Q_OBJECT
+
 public:
     explicit SchedulerGUI(QWidget* parent = nullptr);
 
@@ -18,29 +20,59 @@ private slots:
     void startScheduler();
     void updateSimulation();
     void addProcess();
-    void resetAll();
-    void handleAlgoChange(const QString &algo);
+
+    void generateProcesses();
+
+  
+    void addProcessLive();
 
 private:
     
-    QLineEdit* arrivalInput;
+    // 🔹 Inputs
+    QLineEdit* numProcessesInput;   
 
-    QPushButton* deleteBtn;
-    QLabel* prioLabel;
+    QLineEdit* arrivalInput;
     QLineEdit* burstInput;
     QLineEdit* priorityInput;
+
     QComboBox* algoSelect;
+    QComboBox* modeSelect;   // Live / Static
 
 
+    // 🔹 Outputs
     QLabel* avgWaitingTimeLabel;
     QLabel* avgTurnaroundTimeLabel;
-
 
     QTableWidget* table;
     QGraphicsView* ganttView;
     QGraphicsScene* scene;
 
     
+    // 🔹 Timer
     QTimer* timer;
     int currentStepIndex; 
+
+
+    
+    int currentTime;
+    int lastExecutedPid;
+    int stepStartTime;
+    bool isRunning;
+
+    // Non-Preemptive (SJF + Priority)
+    int currentProcessIndex = -1;
+
+    //  Round Robin
+    std::queue<int> readyQueue;
+    int quantum = 2;
+
+
+    //  Buttons (NEW)
+    QPushButton* pauseBtn;
+    QPushButton* resumeBtn;
+    QPushButton* addProcessLiveBtn;
+
+
+    //  Core function
+    void simulateStep();
 };
